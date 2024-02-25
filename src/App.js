08f8869ca2984e27e2644f1b0a0c5505dc48008f
zhurongxiao <48237338+zhurongxiao@users.noreply.github.com>
2024-02-25 20:32:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}，this new web</div>;
 }
 
 export default App;
