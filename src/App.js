373ddfaf8bfe7e51bc9e1dfd566884ed3aef8b5d
zhurongxiao <350058148@qq.com>
2024-02-25 20:47:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}，this new web</div>;
+  
+  return <div><p>Think of a big, beautiful, empty land with mountains forests, lakes. Animals and fish but no people</p>
+  <p>This was America 16,000 years ago</p>
+  </div>;
+
 }
 
 export default App;
